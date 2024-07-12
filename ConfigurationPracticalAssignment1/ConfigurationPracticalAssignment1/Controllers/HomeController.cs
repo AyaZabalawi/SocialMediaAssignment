a64b19877b8455e1fa1cece1679778145a9eeddb
AyaZabalawi <128138041+AyaZabalawi@users.noreply.github.com>
2024-07-12 11:17:55 +0400

    Add files via upload

---
+++ ConfigurationPracticalAssignment1/ConfigurationPracticalAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.Extensions.Options;
+using ConfigurationPracticalAssignment1;
+
+namespace ConfigurationPracticalAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _options;
+        public HomeController(IOptions<SocialMediaLinksOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.SocialMediaLinks = _options;
+            return View();
+        }
+    }
+}
